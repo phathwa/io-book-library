b042bf905ece5b524e381a25441a83efc2748c38
The Merchant <phathwa@gmail.com>
2024-11-24 00:46:53 +0200

    api/ landing

--- api/auth.py
+++ api/auth.py
@@ -1,25 +1,23 @@
 import os
-from flask import json, request, jsonify
+import json
+from flask import request, jsonify
 from functools import wraps
 
-# Extract the API_KEY environment variable (with a fallback to "fake-key")
 api_key_env = os.getenv("API_KEY", "fake-key")
 
-# If the API_KEY is in JSON format, you can parse it
 try:
     api_key_dict = json.loads(api_key_env)
-    api_key_value = api_key_dict.get("x-api-key", "fake-key")  # Extract 'x-api-key'
+    api_key_value = api_key_dict.get("x-api-key", "fake-key")
 except json.JSONDecodeError:
-    api_key_value = api_key_env  # In case the value is not JSON, use the raw value
+    api_key_value = api_key_env
 
 print(f"====================> {api_key_value}")
 
-# Decorator to require API key for a route
 def require_api_key(f):
     @wraps(f)
     def decorated(*args, **kwargs):
         api_key = request.headers.get('X-API-Key')
-        if api_key and api_key == api_key_value:  # Use api_key_value here
+        if api_key and api_key == api_key_value:
             return f(*args, **kwargs)
         return jsonify({"error": "Invalid API key"}), 401
     return decorated
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import redirect, render_template, url_for
 from api import create_app, db
 
 app = create_app()
@@ -6,6 +6,10 @@ app = create_app()
 @app.route('/', methods=['GET'])
 def main():
     return render_template('index.html')
+
+@app.route('/api', methods=['GET'])
+def api_landing():
+    return redirect(url_for("app.home")) 
 
 if __name__ == "__main__":
     with app.app_context():
